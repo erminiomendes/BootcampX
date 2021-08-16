1561ddfa3c5ecd1031e1efc8bea0d8394ab83fbc
Erminio <Erminio>
2021-08-16 23:33:49 +0000

    2_queries_joins JOINS

--- 2_queries_joins/totalAssDuration.sql
+++ 2_queries_joins/totalAssDuration.sql
@@ -1,3 +1,4 @@
 SELECT sum(assignment_submissions.duration) as total_duration FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
 JOIN assignment_submissions ON students.id = assignment_submissions.student_id
-WHERE students.name = 'Ibrahim Schimmel';
+WHERE cohorts.name = 'FEB12';
