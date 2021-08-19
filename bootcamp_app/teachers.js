ef4416b39c2d9dd15a99d424b9b70ecc64dec63c
Erminio <Erminio>
2021-08-19 05:53:13 +0000

    injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,13 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = '${values[0] || 'JUL02'}'
 ORDER BY teacher;
 `)
 .then(res => {
